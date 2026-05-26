fca2f7682fe7388840e152a49e2e23ff33da8e61
monikanogia <nogiamonika2005@gmail.com>
2026-05-26 13:20:36 +0530

    Database Successfully Connceted
    Signed-off-by: monikanogia <nogiamonika2005@gmail.com>

--- artifacts/menu-app/vite.config.ts
+++ artifacts/menu-app/vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:3001",
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
       },
--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,13 +1,12 @@
 /// <reference types="node" />
-
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
-
+import path from 'path';
 export default defineConfig({
-  schema: './src/schema/index.ts', // Sirf relative path use karein
+  schema: './src/schema/*.ts', // Ye folder 'lib/db/src/schema/' ko point karega
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-// Isse replace karein:
-   url: (process.env.DATABASE_URL as string),
+    url: "postgresql://neondb_owner:npg_gTlf9r0QAqCk@ep-shiny-dust-apcz1n9m.c-7.us-east-1.aws.neon.tech/neondb?sslmode=requireen",
   },
 });

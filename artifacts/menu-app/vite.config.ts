156fd51ab6011fe8f0cbfea89cc69abf346591ac
monikanogia <146133968+monikanogia@users.noreply.github.com>
2026-05-26 15:52:24 +0530

    Update vite.config.ts

--- artifacts/menu-app/vite.config.ts
+++ artifacts/menu-app/vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://sher-e-punjab-1aqj.onrender.com",
         changeOrigin: true,
         secure: false,
       },
